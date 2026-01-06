c96965f2cda8f9e1bb8597df64a39ebd986a2785
Lion <109268357+LionPetr@users.noreply.github.com>
2026-01-06 13:12:04 -0800

    Alternate/proximity collection system (#4)
    * added .vsconfig to gitignore
    
    * rigid body based proximity collection system
    
    * a better system for picking up and storing objects using a parabola type trajectory
    
    * restructured inventory logic to work independently of proximityController logic and added a limit check on the amount of items the inventory can hold
    
    * added a system to check for resource type as well as refactoredinventory AddItem() method to use a preexisting gameobject instead of creating a new one for each item added
    
    * General comments for all files + renaming item class -> resource class for clarity
    
    * updated travel path of resource

---
+++ Assets/Scripts/ProximitySystem/Resource.cs
@@ -0,0 +1,67 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/*
+ * Resource class is responsible for all the things a resource should do 
+ * - has a TravelToInventory method which allows the resource to travel 
+ *   to a position assigned by the inventory 
+ * - contains ResourceData object which contains information about the current resource 
+ * 1/5/2026
+ */
+public class Resource : MonoBehaviour
+{
+    //pos and variables for adjusting resource travel mechanic
+    public GameObject inventorySlot;
+    public float travelTime = 4f;
+    public float arcHeight = 1.0f;
+    public float t;
+    private Vector3 start;
+
+    public ResourceData resourceData;
+    public bool traveling = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (traveling)
+        {
+            TravelToInventory();
+        }
+    }
+
+    public void startTravel()
+    {
+        start = transform.position;
+        t = 0;
+        traveling = true;
+    }
+
+    void TravelToInventory() 
+    {
+        t += Time.deltaTime / travelTime;
+        t = Mathf.Clamp01(t);
+
+        Vector3 end = inventorySlot.transform.position;
+
+        Vector3 linearPos = Vector3.Lerp(start, end, t);
+
+        float height = arcHeight * 4f * (t - t * t);  // Parabola
+
+        Vector3 finalPos = linearPos + Vector3.up * height;
+        transform.position = finalPos;
+        transform.rotation = Quaternion.Slerp(transform.rotation, inventorySlot.transform.rotation, Time.deltaTime * 5f);
+
+        if (t >= 1f)
+        {
+            traveling = false;
+            t = 0;
+        }
+    }
+
+
+}
